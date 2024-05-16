5b5fcfdbc868dca34583126f001d8c88dd328abc
SergioCamiloCastillo <scastillonu@uniminuto.edu.co>
2024-05-15 22:06:31 -0500

    feature: add structure and folder project

---
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {Button} from 'react-native-paper';
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <Button
+        icon="camera"
+        mode="contained"
+        onPress={() => console.log('Pressed')}>
+        Press me
+      </Button>
+    </View>
+  );
+};
